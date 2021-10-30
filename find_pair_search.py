ad59bdb29ed3476e9df8629e68f6dd80c4d69f62
Sem441 <semaao@mail.ru>
2021-10-30 19:11:11 +0500

    30.10 refactor code

--- find_pair_search.py
+++ find_pair_search.py
@@ -1,14 +1,13 @@
 import requests
-import sqlalchemy
-from keys import users_key, sql_pass, sql_login
+import time
+from keys import user_token
+import load_to_database
 
 
-def search_pair(sex, age, city, user):
-    engine = sqlalchemy.create_engine(f"postgresql://{sql_login}:{sql_pass}@localhost:5432/netology")
-    connection = engine.connect()
+def search_pair(sex, age, city):
     url = f"https://api.vk.com/method/users.search?sort=0&v=5.131&fields=sex,bdate,home_town,relation&" \
           f"count=1000&hometown={city}&sex={sex}&status=6&age_from={age}&age_to={age}"
-    full_token = "access_token=" + users_key[user]
+    full_token = "access_token=" + user_token
     request = requests.get(url, full_token)
     print(request.json())
     count = len(request.json()["response"]["items"])
@@ -20,9 +19,27 @@ def search_pair(sex, age, city, user):
         birth_date = request.json()["response"]["items"][number].get("bdate")
         home_town = request.json()["response"]["items"][number].get("home_town")
         relation = request.json()["response"]["items"][number].get("relation")
-        connection.execute(f"""INSERT INTO vk_people VALUES ({number}, {ids}, '{name}', '{last_name}', {search_sex},
-                               '{birth_date}','{home_town}', '{relation}');""")
+        load_to_database.load_data(ids, name, last_name, search_sex, birth_date, home_town, relation)
+
+
+def user_info(user):
+    url = f"https://api.vk.com/method/users.get?user_id={user}&v=5.131&fields=sex,bdate,home_town, city"
+    token = f"access_token={user_token}"
+    request = requests.get(url, token)
+    sex = request.json()["response"][0].get("sex")
+    if sex == 1:
+        sex = 2
+    else:
+        sex = 1
+    # sex = None
+    age = request.json()["response"][0].get("bdate")
+    current_data = time.strftime("%Y")
+    age = int(current_data)-int(age[-4:])
+    # age = None
+    city = request.json()["response"][0].get("home_town")
+    # city = None
+    return sex, age, city
 
 
 if __name__ == '__main__':
-    search_pair("", "", "", "")
+    search_pair("", "", "")
--- main.py
+++ main.py
@@ -4,46 +4,61 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 import keys
-import take_user_parametrs
 import find_pair_search
-import missing_data
 
 vk = vk_api.VkApi(token=keys.group_token)
 longpoll = VkLongPoll(vk)
 
-open_chats = [663114110]
+open_chats = []
+welcome_prefix = ("прив", "Прив", "Добр", "добр", "Здра", "здра")
+mens_prefix = ("м", "М", "муж", "Муж", "МУЖ")
+women_prefix = ("ж", "Ж", "Жен", "ЖЕН", "жен")
 
 
 def write_msg(user_id, message):
     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7)})
 
 
+def add_data(user_id, message):
+    info_msg = False
+    for question in longpoll.listen():
+        if question.type == VkEventType.MESSAGE_NEW:
+            if not info_msg:
+                write_msg(user_id, message)
+                info_msg = True
+            msg = question.text
+            if question.to_me and msg.startswith(mens_prefix):
+                return 2
+            elif question.to_me and msg.startswith(women_prefix):
+                return 1
+            elif question.to_me and msg.isdigit():
+                return msg
+            elif question.to_me and msg.isalpha():
+                return msg
+
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:
 
-        if event.to_me and event.user_id not in open_chats:
-            write_msg(event.user_id, "Добрый день, введите токен")
-            open_chats.append(event.user_id)
-
-        if event.to_me and event.user_id in open_chats and len(event.text) == 85:
-            print(event.text)
-            keys.users_key[event.user_id] = event.text
-            answer = take_user_parametrs.user_info(event.user_id)
+        if event.to_me:
+            request = event.text
+            if event.user_id not in open_chats and request.startswith(welcome_prefix):
+                write_msg(event.user_id, "Добрый день")
+                open_chats.append(event.user_id)
+            answer = find_pair_search.user_info(event.user_id)
             sex = answer[0]
             if sex is None:
-                sex = missing_data.add_data(event.user_id, "Введите пол поиска м/ж")
+                sex = add_data(event.user_id, "Введите пол поиска м/ж")
             age = answer[1]
             if age is None:
-                age = missing_data.add_data(event.user_id, "Укажите возраст")
+                age = add_data(event.user_id, "Укажите возраст")
             city = answer[2]
             if city is None:
-                city = missing_data.add_data(event.user_id, "Укажите город")
-
-            find_pair_search.search_pair(sex, age, city, event.user_id)
+                city = add_data(event.user_id, "Укажите город")
+            find_pair_search.search_pair(sex, age, city)
             write_msg(event.user_id, "Данные успешно записаны в БД")
-
         elif event.text.lower() == "пока":
             write_msg(event.user_id, "До свидания")
             open_chats.pop(event.user_id)
-            # else:
-            #     write_msg(event.user_id, "Не поняла вашего ответа...")
+        else:
+            write_msg(event.user_id, "Не поняла вашего ответа...")
